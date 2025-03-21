9bed20db9772dd0950085657f77e1c2db58e8eb4
Ubuntu <ubuntu@ip-172-31-24-92.us-west-1.compute.internal>
2025-03-21 23:05:00 +0000

    packagised gpatlas

---
+++ workflow_sims/gpatlas/src/gpatlas/__init__.py
@@ -0,0 +1,23 @@
+"""
+GPatlas: Genetic data analysis toolkit with autoencoder models.
+
+This package provides tools for analyzing genetic data using deep learning models,
+particularly focused on genotype-phenotype relationships.
+"""
+
+__version__ = "0.1.0"
+
+# Import commonly used functions and classes for easier access
+from .datasets import BaseDataset, GenoPhenoDataset, GenoDataset, PhenoDataset, create_data_loaders
+from .models import *
+"""
+from .training import (
+train_localgg_model,
+train_pp_model,
+train_gp_model,
+focal_loss_for_genetic_data,
+)
+from .optimization import run_optimization
+
+# Constants that can be overridden by the user
+from .config import DEFAULT_CONFIG """
