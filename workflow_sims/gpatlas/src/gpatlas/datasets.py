cb9c31853ab06b70e18ca7e9956ea6a64cb1f1ed
Ubuntu <ubuntu@ip-172-31-24-92.us-west-1.compute.internal>
2025-04-03 22:43:27 +0000

    Implement lazy file handle initialization in BaseDataset

--- workflow_sims/gpatlas/src/gpatlas/datasets.py
+++ workflow_sims/gpatlas/src/gpatlas/datasets.py
@@ -17,12 +17,24 @@ class BaseDataset(Dataset):
         hdf5_path: Path to the HDF5 file containing the data
     """
     def __init__(self, hdf5_path: Path) -> None:
-        self.h5 = h5py.File(hdf5_path, "r")
-        self._strain_group = cast(h5py.Group, self.h5["strains"])
-        self.strains: List[str] = list(self._strain_group.keys())
+        self.hdf5_path = hdf5_path
+        self.h5 = None
+        self._strain_group = None
+        self.strains = None
+        # Open temporarily to get keys and length for initialization
+        with h5py.File(self.hdf5_path, "r") as temp_h5:
+            temp_strain_group = cast(h5py.Group, temp_h5["strains"])
+            self._strain_keys: List[str] = list(temp_strain_group.keys())
+            self._len = len(temp_strain_group)
+
+    def _init_h5(self):
+        if self.h5 is None:
+            self.h5 = h5py.File(self.hdf5_path, "r")
+            self._strain_group = cast(h5py.Group, self.h5["strains"])
+            self.strains = self._strain_keys
 
     def __len__(self) -> int:
-        return len(self._strain_group)
+        return self._len
 
 
 class GenoPhenoDataset(BaseDataset):
@@ -32,6 +44,7 @@ class GenoPhenoDataset(BaseDataset):
     Returns tuples of (phenotype, genotype) tensors.
     """
     def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        self._init_h5()
         strain = self.strains[idx]
         strain_data = cast(Dataset, self._strain_group[strain])
 
@@ -48,12 +61,28 @@ class PhenoDataset(BaseDataset):
     Returns phenotype tensors.
     """
     def __getitem__(self, idx: int) -> torch.Tensor:
+        self._init_h5()
         strain = self.strains[idx]
         strain_data = cast(Dataset, self._strain_group[strain])
 
         phens = torch.tensor(strain_data["phenotype"][:], dtype=torch.float32)
 
         return phens
+
+class GenoPhenoBVDataset(BaseDataset):
+    """
+    Dataset for loading genotype, phenotype and breeding value data.
+    """
+    def __getitem__(self, idx: int):
+        self._init_h5()
+        strain = self.strains[idx]
+        strain_data = cast(Dataset, self._strain_group[strain])
+
+        phens = torch.tensor(strain_data["phenotype"][:], dtype=torch.float32)
+        phens_bv = torch.tensor(strain_data["phenotypes_bv"][:], dtype=torch.float32)
+        gens = torch.tensor(strain_data["genotype"][:], dtype=torch.float32).flatten()
+
+        return phens, phens_bv, gens
 
 
 class GenoDataset(BaseDataset):
@@ -63,6 +92,7 @@ class GenoDataset(BaseDataset):
     Returns genotype tensors.
     """
     def __getitem__(self, idx: int) -> torch.Tensor:
+        self._init_h5()
         strain = self.strains[idx]
         strain_data = cast(Dataset, self._strain_group[strain])
 
@@ -94,6 +124,9 @@ def create_data_loaders(base_file_name, batch_size=128, num_workers=3, shuffle=T
     train_data_pheno = PhenoDataset(Path(f'{base_file_name}train.hdf5'))
     test_data_pheno = PhenoDataset(Path(f'{base_file_name}test.hdf5'))
 
+    train_data_gp_bv = GenoPhenoBVDataset(Path(f'{base_file_name}train.hdf5'))
+    test_data_gp_bv = GenoPhenoBVDataset(Path(f'{base_file_name}test.hdf5'))
+
     # Create DataLoaders
     train_loader_geno = torch.utils.data.DataLoader(
         dataset=train_data_geno, batch_size=batch_size,
@@ -122,6 +155,15 @@ def create_data_loaders(base_file_name, batch_size=128, num_workers=3, shuffle=T
         num_workers=num_workers, shuffle=shuffle
     )
 
+    train_loader_gp_bv = torch.utils.data.DataLoader(
+        dataset=train_data_gp_bv, batch_size=batch_size,
+        num_workers=num_workers, shuffle=shuffle
+    )
+    test_loader_gp_bv = torch.utils.data.DataLoader(
+        dataset=test_data_gp_bv, batch_size=batch_size,
+        num_workers=num_workers, shuffle=shuffle
+    )
+
     return {
         'train_loader_geno': train_loader_geno,
         'test_loader_geno': test_loader_geno,
@@ -129,4 +171,6 @@ def create_data_loaders(base_file_name, batch_size=128, num_workers=3, shuffle=T
         'test_loader_pheno': test_loader_pheno,
         'train_loader_gp': train_loader_gp,
         'test_loader_gp': test_loader_gp,
+        'train_loader_gp_bv': train_loader_gp_bv,
+        'test_loader_gp_bv': test_loader_gp_bv,
     }
