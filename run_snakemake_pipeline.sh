3c50ae4e20f2d61d433def5296871a7d7bcd3909
Ubuntu <ubuntu@ip-172-31-24-92.us-west-1.compute.internal>
2025-05-27 22:31:01 +0000

    fixed missing snakepipe

--- run_snakemake_pipeline.sh
+++ run_snakemake_pipeline.sh
@@ -9,6 +9,7 @@ cd ../alphasimr_dilution/
 snakemake -s Snakefile_sims.smk --use-conda --cores 1 -n
 snakemake -s Snakefile_gpatlas.smk --use-conda --cores 1 -n
 snakemake -s Snakefile_linear_mod.smk --use-conda --cores 1 -n
+snakemake -s Snakefile_feat_seln.smk --use-conda --cores 1 -n
 
 cd ../alphasimr_pleio/
 snakemake -s Snakefile_sims.smk --use-conda --cores 1 -n
