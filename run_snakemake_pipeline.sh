1b2ae6c41211b19d8dd43620561447dc98389f36
Ubuntu <ubuntu@ip-172-31-24-92.us-west-1.compute.internal>
2025-06-03 23:20:49 +0000

    cd back to master dir of repo

--- run_snakemake_pipeline.sh
+++ run_snakemake_pipeline.sh
@@ -15,3 +15,5 @@ cd ../alphasimr_pleio/
 snakemake -s Snakefile_sims.smk --use-conda --cores 1
 snakemake -s Snakefile_gpatlas.smk --use-conda --cores 1
 snakemake -s Snakefile_linear_mod.smk --use-conda --cores 1
+
+cd ../..
